8160b51788edab228f3652cd8abf563dbb6b2d5b
Irina Pesheva <tayrin444@gmail.com>
2021-04-04 17:31:47 +0300

    Add 404 page for main_page
    Just in case
    
    See #6

--- program part/vg_site/main_page/views.py
+++ program part/vg_site/main_page/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from pages_info.models import PageInfo
 
 
 def index(request, pagename):
     pagename = '/' + pagename
-    pg_info = PageInfo.objects.get(permalink=pagename)
+    # pg_info = PageInfo.objects.get(permalink=pagename)
+    pg_info = get_object_or_404(PageInfo, permalink=pagename)
     context = {
         'title': pg_info.title,
         'nav_title': pg_info.nav_title,
