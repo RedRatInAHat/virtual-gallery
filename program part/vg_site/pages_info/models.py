7829cb915bd42ebf4df4f91c587e58c375e0b4a7
Irina Pesheva <tayrin444@gmail.com>
2021-04-04 11:03:47 +0300

    Add model
    See #9

--- program part/vg_site/pages_info/models.py
+++ program part/vg_site/pages_info/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class PageInfo(models.Model):
+    title = models.CharField('Title', max_length=60)
+    nav_title = models.CharField('Navigation Title', max_length=60)
+    permalink = models.CharField('Permalink', max_length=20, unique=True)
+    update_date = models.DateTimeField('Last Updated')
+    main_text = models.TextField('Main Text', blank=True)
