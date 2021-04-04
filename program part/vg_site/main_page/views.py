e6e4f1071f32301c287cf4c3ed3f3c6309c615c6
Irina Pesheva <tayrin444@gmail.com>
2021-04-04 15:17:21 +0300

    Add variables to view output
    See #12

--- program part/vg_site/main_page/views.py
+++ program part/vg_site/main_page/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from pages_info.models import PageInfo
 
 
-def index(request):
-    return render(request, 'main_page/main.html')
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg_info = PageInfo.objects.get(permalink=pagename)
+    context = {
+        'title': pg_info.title,
+        'nav_title': pg_info.nav_title,
+        'content': pg_info.main_text,
+        'pages_list': PageInfo.objects.all(),
+    }
+    return render(request, 'main_page/main.html', context)
