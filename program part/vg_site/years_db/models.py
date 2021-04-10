635c1b1d3115dfa78330cd0475b2a4b1193096cb
Irina Pesheva <tayrin444@gmail.com>
2021-04-10 16:53:15 +0300

    Change themes table settings
    Permalink isn't unique for now
    
    See #23

--- program part/vg_site/years_db/models.py
+++ program part/vg_site/years_db/models.py
@@ -11,7 +11,7 @@ class YearsInfo(models.Model):
 class ThemesInfo(models.Model):
     title = models.CharField('Title', max_length=60)
     nav_title = models.CharField('Navigation Title', max_length=60)
-    permalink = models.CharField('Permalink', max_length=20, unique=True)
+    permalink = models.CharField('Permalink', max_length=20)
     year = models.CharField("Year Title", max_length=60)
     main_text = models.TextField('Main Text', blank=True)
     update_date = models.DateTimeField('Last Updated', auto_now=True)
