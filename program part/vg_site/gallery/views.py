ec29a7d1095765ee47d1ab7ad48d3ede8a4672d2
Irina Pesheva <tayrin444@gmail.com>
2021-04-10 12:43:20 +0300

    Add view for gallery
    See #13

--- program part/vg_site/gallery/views.py
+++ program part/vg_site/gallery/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from pages_info.models import PageInfo
 
-# Create your views here.
+
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg_info = get_object_or_404(PageInfo, permalink=pagename)
+    context = {
+        'title': pg_info.title,
+        'nav_title': pg_info.nav_title,
+        'content': pg_info.main_text,
+        'pages_list': PageInfo.objects.all(),
+    }
+    return render(request, 'gallery/main_page.html', context)
