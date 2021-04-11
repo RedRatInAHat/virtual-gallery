85e8e29f66e7e68218df13c9bf4482dc4eb4da91
Irina Pesheva <tayrin444@gmail.com>
2021-04-11 13:33:53 +0300

    Change FileField to ImageField in images model
    See #25

--- program part/vg_site/images/models.py
+++ program part/vg_site/images/models.py
@@ -5,6 +5,6 @@ class ImagesInfo(models.Model):
     title = models.CharField('Title', max_length=60)
     year_link = models.CharField('Year', max_length=60)
     theme_link = models.CharField('Theme', max_length=60, blank=True)
-    image_file = models.FileField('File', upload_to='uploaded_images/')
+    image_file = models.ImageField('File', upload_to='uploaded_images/')
     creation_date = models.DateField('Date of creation', blank=True)
     description = models.TextField('Description', blank=True)
