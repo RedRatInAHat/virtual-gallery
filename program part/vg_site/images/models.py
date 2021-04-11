ddddcc0216ce8a7c7dce350c38a72c882b102bec
Irina Pesheva <tayrin444@gmail.com>
2021-04-11 13:22:21 +0300

    Add model for images
    See #25

--- program part/vg_site/images/models.py
+++ program part/vg_site/images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ImagesInfo(models.Model):
+    title = models.CharField('Title', max_length=60)
+    year_link = models.CharField('Year', max_length=60)
+    theme_link = models.CharField('Theme', max_length=60, blank=True)
+    image_file = models.FileField('File', upload_to='uploaded_images/')
+    creation_date = models.DateField('Date of creation', blank=True)
+    description = models.TextField('Description', blank=True)
