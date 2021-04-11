f435b09bb1460ebf575ef32a0fa9ebdfa83c6712
Irina Pesheva <tayrin444@gmail.com>
2021-04-11 12:00:15 +0300

    Add themes view to urls
    See #23

--- program part/vg_site/gallery/urls.py
+++ program part/vg_site/gallery/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('gallery', views.index, {'pagename': 'gallery'}, name='home'),
-               path('gallery/<str:year>/', views.get_year_info, name='year')]
+               path('gallery/<str:year>/', views.get_year_info, name='year'),
+               path('gallery/<str:year>/<str:theme>', views.get_theme_info, name='theme')]
