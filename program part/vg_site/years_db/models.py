aa469f935dc9a25ca152232b5c291acaf3861717
Irina Pesheva <tayrin444@gmail.com>
2021-04-11 16:41:11 +0300

    Add choices to themes
    See #26

--- program part/vg_site/years_db/models.py
+++ program part/vg_site/years_db/models.py
@@ -9,9 +9,11 @@ class YearsInfo(models.Model):
 
 
 class ThemesInfo(models.Model):
+    year_choices = {(year.title, year.permalink[1:-1]) for year in
+                    YearsInfo.objects.all().order_by('permalink')}
     title = models.CharField('Title', max_length=60)
     nav_title = models.CharField('Navigation Title', max_length=60)
     permalink = models.CharField('Permalink', max_length=20)
-    year = models.CharField("Year Title", max_length=60)
+    year = models.CharField("Year Title", max_length=60, choices=year_choices)
     main_text = models.TextField('Main Text', blank=True)
     update_date = models.DateTimeField('Last Updated', auto_now=True)
