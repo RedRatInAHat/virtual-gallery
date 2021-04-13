d5815e4d48a55e3b22f921d24551fe1d98891122
Irina Pesheva <tayrin444@gmail.com>
2021-04-13 15:49:24 +0300

    Change representation of enlarged image
    See #43

--- program part/vg_site/vg_site/static/modal_image.js
+++ program part/vg_site/vg_site/static/modal_image.js
@@ -2,6 +2,7 @@ function display_image(image) {
   document.getElementById("gallery_image_modal").style.display = "block";
   document.getElementById("enlarged_image").src = image.src;
   document.getElementById("image_modal_title").innerHTML = image.alt;
+  document.body.style.overflow = "hidden";
 }
 
 function display_message(){
@@ -11,4 +12,5 @@ function display_message(){
 function span_click() {
   var modal = document.getElementById("gallery_image_modal");
   modal.style.display = "none";
+  document.body.style.overflow = "visible";
 }
