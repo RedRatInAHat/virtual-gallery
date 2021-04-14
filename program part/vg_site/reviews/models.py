bd6d57acc20617688889562bbaedad2ae550a9ff
Irina Pesheva <tayrin444@gmail.com>
2021-04-14 14:55:08 +0300

    Add review model
    See #30

--- program part/vg_site/reviews/models.py
+++ program part/vg_site/reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Reviews(models.Model):
+    review_type_choices = {('review', 'review'), ('suggestion', 'suggestion')}
+    review_type = models.CharField("Review Type: ", max_length=10, choices=review_type_choices)
+    review_text = models.TextField("Text: ")
+    name = models.CharField('From: ', max_length=60, default='Unknown')
