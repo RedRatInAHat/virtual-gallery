ae3f68fd35a6382f3a04a3ace9cf489274265672
Irina Pesheva <tayrin444@gmail.com>
2021-04-14 17:56:10 +0300

    Add 'active' and 'creation_date' to model
    See #30

--- program part/vg_site/reviews/models.py
+++ program part/vg_site/reviews/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 
 class Reviews(models.Model):
+    
     review_type_choices = {('review', 'review'), ('suggestion', 'suggestion')}
+
     review_type = models.CharField("Review Type: ", max_length=10, choices=review_type_choices)
     review_text = models.TextField("Text: ")
-    name = models.CharField('From: ', max_length=60, default='Unknown')
+    name = models.CharField("From: ", max_length=60, default='Unknown')
+    active = models.BooleanField("Active", default=True)
+    creation_date = models.DateTimeField("Date of creation", auto_now=True)
