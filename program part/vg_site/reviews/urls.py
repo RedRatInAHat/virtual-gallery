f0e623a996a64759a58f923bcc78486db491d4be
Irina Pesheva <tayrin444@gmail.com>
2021-04-15 11:40:59 +0300

    Add page view for ready reviews
    See #30

--- program part/vg_site/reviews/urls.py
+++ program part/vg_site/reviews/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('reviews', views.review_main, {'pagename': 'reviews'}, name='home'), ]
+urlpatterns = [path('reviews', views.review_main, {'pagename': 'reviews'}, name='home'),
+               path('reviews/read', views.review_read, {'pagename': 'reviews', 'mode': 'read'}, name='read'), ]
--- program part/vg_site/reviews/views.py
+++ program part/vg_site/reviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from pages_info.models import PageInfo
-from reviews.models import ReviewsPages
+from reviews.models import ReviewsPages, Reviews
 
 
 def review_main(request, pagename):
@@ -14,3 +14,18 @@ def review_main(request, pagename):
         'reviews_pages_list': ReviewsPages.objects.all(),
     }
     return render(request, 'reviews/main_page.html', context)
+
+
+def review_read(request, pagename, mode):
+    pagename = '/' + pagename
+    mode = '/' + mode
+    pg_info = get_object_or_404(ReviewsPages, permalink=mode)
+    context = {
+        'title': pg_info.title,
+        'content': pg_info.main_text,
+        'permalink': pagename,
+        'pages_list': PageInfo.objects.all(),
+        'reviews_pages_list': ReviewsPages.objects.all(),
+        'reviews': Reviews.objects.filter(active=True, review_type='review'),
+    }
+    return render(request, 'reviews/read_page.html', context)
