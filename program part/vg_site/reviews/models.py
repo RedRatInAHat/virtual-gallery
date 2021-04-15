185785173969b021aac05b7df37668a22b0f4f7c
Irina Pesheva <tayrin444@gmail.com>
2021-04-15 10:25:32 +0300

    Add reviews pages module
    See #30

--- program part/vg_site/reviews/models.py
+++ program part/vg_site/reviews/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Reviews(models.Model):
-    
     review_type_choices = {('review', 'review'), ('suggestion', 'suggestion')}
 
     review_type = models.CharField("Review Type: ", max_length=10, choices=review_type_choices)
@@ -10,3 +9,11 @@ class Reviews(models.Model):
     name = models.CharField("From: ", max_length=60, default='Unknown')
     active = models.BooleanField("Active", default=True)
     creation_date = models.DateTimeField("Date of creation", auto_now=True)
+
+
+class ReviewsPages(models.Model):
+    title = models.CharField('Title', max_length=60)
+    permalink = models.CharField('Permalink', max_length=20, unique=True)
+    nav_title = models.CharField('Navigation Title', max_length=60)
+    main_text = models.TextField('Main Text', blank=True)
+    update_date = models.DateTimeField('Last Updated', auto_now=True)
