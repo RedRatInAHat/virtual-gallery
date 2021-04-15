6375e1e245c5785a8d1cefcf5e1f6da400f4b983
Irina Pesheva <tayrin444@gmail.com>
2021-04-15 15:05:19 +0300

    Add write review view
    See #30

--- program part/vg_site/reviews/urls.py
+++ program part/vg_site/reviews/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('reviews', views.review_main, {'pagename': 'reviews'}, name='home'),
-               path('reviews/read', views.review_read, {'pagename': 'reviews', 'mode': 'read'}, name='read'), ]
+               path('reviews/read', views.review_read, {'pagename': 'reviews', 'mode': 'read'}, name='read'),
+               path('reviews/write', views.review_write, {'pagename': 'reviews', 'mode': 'write'}, name='write'),
+               path('reviews/write/', views.review_write, {'pagename': 'reviews', 'mode': 'write'},
+                    name='submit-write'), ]
--- program part/vg_site/reviews/views.py
+++ program part/vg_site/reviews/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from pages_info.models import PageInfo
 from reviews.models import ReviewsPages, Reviews
+from .forms import ReviewsForm
+from django.http import HttpResponseRedirect
 
 
 def review_main(request, pagename):
@@ -29,3 +31,31 @@ def review_read(request, pagename, mode):
         'reviews': Reviews.objects.filter(active=True, review_type='review'),
     }
     return render(request, 'reviews/read_page.html', context)
+
+
+def review_write(request, pagename, mode):
+    pagename = '/' + pagename
+    mode = '/' + mode
+    submitted = False
+
+    if request.method == 'POST':
+        form = ReviewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(pagename + mode + '/?submitted=True')
+    else:
+        form = ReviewsForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    pg_info = get_object_or_404(ReviewsPages, permalink=mode)
+    context = {
+        'title': pg_info.title,
+        'content': pg_info.main_text,
+        'permalink': pagename,
+        'pages_list': PageInfo.objects.all(),
+        'reviews_pages_list': ReviewsPages.objects.all(),
+        'reviews': Reviews.objects.filter(active=True, review_type='review'),
+        'submitted': submitted,
+        'form': form,
+    }
+    return render(request, 'reviews/write_page.html', context)
